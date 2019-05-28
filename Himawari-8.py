105e4b8272a88714e1a1918d42e4509e78ed9956
zzllg <296160709@qq.com>
2019-05-28 17:11:06 +0800

    Update Himawari-8.py

--- Himawari-8.py
+++ Himawari-8.py
@@ -10,20 +10,21 @@ from matplotlib import pyplot as plt
 from mpl_toolkits.basemap import Basemap
 from mpl_toolkits.basemap import maskoceans
 import warnings
+import struct
 # import struct
 warnings.filterwarnings('ignore')
 np.set_printoptions(suppress= True)
 
 
-# In[2]:
+# In[94]:
 
 
-filename = 'D:\Himawari\AHI8_OBI_4000M_NOM_20190223_0020.hdf'
+filename = 'D:\Himawari\AHI8_OBI_4000M_NOM_20190222_2150.hdf'
 path_out = 'D:\Himawari\out'
 filename_out = 'AHI8_FOG_' + filename.split('_')[4] + '_' + filename.split('_')[5].split('.')[0]
 
 
-# In[3]:
+# In[95]:
 
 
 def get_LonLat(l,c):
@@ -34,7 +35,7 @@ def get_LonLat(l,c):
     ea = 6378.137
     eb = 6356.7523
     h = 42164
-    lamda_Himawari_8 = 140.7
+    lamda_Himawari_8 = 140.68
     x = (np.pi * (c - coff))/(180 * np.exp2(-16) * cfac)
     y = (np.pi * (l - coff))/(180 * np.exp2(-16) * lfac)
     sd = np.sqrt((h * np.cos(x) * np.cos(y))**2 - (np.cos(y)**2 + ea**2/eb**2 * np.sin(y)**2) * ((h**2) - ea**2))
@@ -47,6 +48,34 @@ def get_LonLat(l,c):
     lat = (180/np.pi)*np.arctan((ea**2/eb**2) * (s3/sxy))
     return lon,lat
 
+def get_CL(lon,lat):
+    coff = 1375.5
+    loff = 1375.5
+    cfac = 10233128
+    lfac = 10233128
+    ea = 6378.137
+    eb = 6356.7523
+    h = 42164
+    lambda_D = 140.7
+    lon = lon*np.pi/180
+    lat = lat*np.pi/180
+    lambda_e = lon
+    phi_e = np.arctan(eb**2 * np.tan(lat)/ea**2)
+    re = eb/np.sqrt(1-(ea**2 - eb**2) * np.cos(phi_e)**2/ea**2)
+    r1 = h - re * np.cos(phi_e) * np.cos(lambda_e - lambda_D)
+    r2 = - re * np.cos(phi_e) * np.sin(lambda_e - lambda_D)
+    r3 = re * np.sin(phi_e)
+    rn = np.sqrt(r1**2 + r2**2 + r3**2)
+    x = np.arctan(-r2/r1)*180/np.pi
+    y = np.arcsin(-r3/rn)*180/np.pi
+    c = coff + x * 2**(-16) * cfac
+    l = loff + y * 2**(-16) * lfac
+    return l,c
+    
+def fidx(data,idx):
+    arr = np.array([data[i,j] for i,j in idx],dtype= 'float32')
+    return arr.reshape((1250,1750))
+    
 def FDI(T,R):
     one = 299792458/(np.pi * 3.9**5)
     two = np.exp(14388 / (3.9 * T) - 1)
@@ -56,24 +85,40 @@ def cosR(sunzenith,R):
     return R/ np.cos(np.deg2rad(sunzenith))
 
 
-# In[4]:
+# In[96]:
+
+
+lons,lats = np.meshgrid(np.arange(70,140,0.04),np.arange(60.,10.,-0.04))
+
+
+# In[97]:
+
+
+index_l,index_c = np.round(get_CL(lons,lats))
+index_l = index_l.astype('uint16')
+index_c = index_c.astype('uint16')
+z = list(zip(index_l.reshape(-1),index_c.reshape(-1)))
+
+
+# In[98]:
 
 
 # open file
 data_himawari8 = h5py.File(filename,'r')
 
 # get bt value
-up = 110
-down = 1120
-left = 0
-right = 1370
-data_39 = data_himawari8['NOMChannelIRX0390_4000'][up:down,left:right] * 0.01
-data_112 = data_himawari8['NOMChannelIRX1120_4000'][up:down,left:right]* 0.01
-data_064 = data_himawari8['NOMChannelVIS0064_4000'][up:down,left:right] * 0.0001
-sunzenith = data_himawari8['NOMSunZenith'][up:down,left:right]
+data_39 = data_himawari8['NOMChannelIRX0390_4000'][:] * 0.01
+data_39 = fidx(data_39,z)
+data_112 = data_himawari8['NOMChannelIRX1120_4000'][:]* 0.01
+data_112 = fidx(data_112,z)
+data_064 = data_himawari8['NOMChannelVIS0064_4000'][:] * 0.0001
+data_064 = fidx(data_064,z)
+sunzenith = data_himawari8['NOMSunZenith'][:]
+sunzenith = fidx(sunzenith,z)
 
 # close
 data_himawari8.close()
+del z,data_himawari8
 
 mask = np.full(sunzenith.shape, True)
 DCD = np.full(sunzenith.shape,999,dtype='float16')
@@ -82,7 +127,7 @@ masked_sunzenith = np.ma.array(sunzenith, mask=np.where(sunzenith == 65535, True
 data_064 = cosR(masked_sunzenith,data_064)
 
 
-# In[5]:
+# In[99]:
 
 
 # 0 - 90 lands
@@ -93,6 +138,10 @@ FDI_039 = FDI(np.ma.array(data_39,mask= mask),np.ma.array(data_064,mask= mask))
 mask[np.where(FDI_039 >= 8)] = True
 FDI_039 = np.ma.array(FDI_039,mask= mask)
 
+
+# In[100]:
+
+
 # 90 + lands
 DCD = np.full(sunzenith.shape,999,dtype= 'float16') #先重置，上步骤用过了
 index = np.where((masked_sunzenith > 90) & (masked_sunzenith <= 180))
@@ -101,26 +150,20 @@ mask[np.where((DCD <= 0) & (mask == True))] = False
 DCD = np.ma.array(DCD,mask= mask)
 
 
-# In[6]:
+# In[101]:
 
 
 # 暂时没有雾的分级，所以直接用mask绘图
 fog_lands = np.where(mask == False,100,0)
 fog_lands = np.ma.array(fog_lands,mask= mask)
-# lon,lat
-lat, lon = np.meshgrid(range(left,right),range(up,down))
-lat = np.ma.array(lat,mask= mask)
-lon = np.ma.array(lon,mask= mask)
-lons_1, lats_1 = get_LonLat(lon,lat)
-lons_1[lons_1 > 180] -= 360
 # ma oceans
-fog_lands = maskoceans(lons_1,lats_1,fog_lands,inlands= True)
+fog_lands = maskoceans(lons,lats,fog_lands,inlands= True)
 
-lons_1 = np.ma.array(lons_1,mask= fog_lands.mask)
-lats_1 = np.ma.array(lats_1,mask= fog_lands.mask)
+lons_1 = np.ma.array(lons,mask= fog_lands.mask)
+lats_1 = np.ma.array(lats,mask= fog_lands.mask)
 
 
-# In[25]:
+# In[102]:
 
 
 # oceans
@@ -142,7 +185,7 @@ mask[np.where((DCD <= 10) & (DCD >= 3))] = False
 index = np.where((data_064 >= 0.2) & (data_064 <= 0.45) &  ( masked_sunzenith >15 ) & (masked_sunzenith <= 80))
 DCD[index] = data_39[index] - data_112[index]
 mask[np.where((DCD <= 20) & (DCD >= 3))] = False
-boxs = np.array([data_064[x_ - 1 : x_ + 2,y_ - 1 : y_ + 2].reshape(9) if x_ > 0 and x_ < 1009 and y_ > 0 and y_ < 1369 else np.ones(9) for x_,y_ in zip(index[0],index[1])])
+boxs = np.array([data_064[x_ - 1 : x_ + 2,y_ - 1 : y_ + 2].reshape(9) if x_ > 0 and x_ < 1248 and y_ > 0 and y_ < 1748 else np.ones(9) for x_,y_ in zip(index[0],index[1])])
 boxs_std = boxs.std(axis= 0)
 mask[np.where((boxs_std < 0.5)|(boxs_std > 3.5))] = True
 
@@ -157,27 +200,21 @@ DCD[index] = data_39[index] - data_112[index]
 mask[np.where((DCD <= -2))] = False
 
 
-# In[26]:
+# In[103]:
 
 
-# lon,lat
-lat, lon = np.meshgrid(range(left,right),range(up,down))
-lat = np.ma.array(lat,mask= mask)
-lon = np.ma.array(lon,mask= mask)
-lons_2, lats_2 = get_LonLat(lon,lat)
-lons_2[lons_2 > 180] -= 360
 # ma lands
-masklands = (maskoceans(lons_2,lats_2,masked_sunzenith,inlands= True).mask == False)
+masklands = (maskoceans(lons,lats,masked_sunzenith,inlands= True).mask == False)
 masklands = masklands | mask
 # 暂时没有雾的分级，所以直接用mask绘图
 fog_oceans = np.where(mask == False,100,0)
 fog_oceans = np.ma.array(fog_oceans,mask= masklands)
 
-lons_2 = np.ma.array(lons_2,mask= fog_oceans.mask)
-lats_2 = np.ma.array(lats_2,mask= fog_oceans.mask)
+lons_2 = np.ma.array(lons,mask= fog_oceans.mask)
+lats_2 = np.ma.array(lats,mask= fog_oceans.mask)
 
 
-# In[27]:
+# In[104]:
 
 
 # 合并
@@ -189,26 +226,13 @@ fog = np.where((fog_lands.mask == True)&(fog_oceans.mask == True), 9999, DCD)
 fog = np.where((fog == 9999)&(sunzenith != 65535), 0, fog)
 
 
-# In[36]:
-
-
-with open(path_out + '\\' + filename_out + '.bin','wb') as f:
-    np.array('test').tofile(f)
-    fog.data.tofile(f)
-
-
-# In[29]:
+# In[105]:
 
 
 fog = np.ma.masked_where((fog == 9999)|(fog == 0),fog)
 
-lat, lon = np.meshgrid(range(left,right),range(up,down))
-lat = np.ma.array(lat,mask= fog.mask)
-lon = np.ma.array(lon,mask= fog.mask)
-lons, lats = get_LonLat(lon,lat)
 
-
-# In[30]:
+# In[106]:
 
 
 # 画图
@@ -224,16 +248,16 @@ m_1.drawparallels(np.arange(10.,61.,10.),labels=[1, 0, 0, 0],fontsize=10)
 X,Y = m_1(lons,lats)
 cs = m_1.contourf(X,Y,fog)
 
-fig.savefig(path_out + '\\' + filename_out + '.jpg')
-plt.show()
+# fig.savefig(path_out + '\\' + filename_out + '.jpg')
 
 
-# In[87]:
+# In[72]:
 
 
-# f_fog = h5py.File(path_out + '\\' + filename_out + '.hdf','w')
-# f_fog['fog'] = fog
-# f_fog['lons'] = lons
-# f_fog['lats'] = lats
-# f_fog.close() 
+with open(path_out + '\\' + filename_out + '.bin','wb') as f:
+    s = 'filename:' + filename_out + 'time:' + filename_out[9:] + 'lon:70E-140E' + 'lat:10N-60N' + 'resolution:0.04*0.04'
+    for i in range(len(s)):
+        s_bin = struct.pack('c',s[i].encode('ASCII'))
+        f.write(s_bin)
+    fog.data.tofile(f)
 
